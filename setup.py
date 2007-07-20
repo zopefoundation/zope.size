ca2afcdce50a62972bc5ad523ee32f509c9a44cf
Fred Drake <fdrake@acm.org>
2007-07-20 03:12:31 +0000

    bump version number for next release

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ import os
 from setuptools import setup, find_packages
 
 setup(name='zope.size',
-      version = '3.4.0',
+      version = '3.4.1',
       url='http://svn.zope.org/zope.size',
       license='ZPL 2.1',
       description='Zope size',
