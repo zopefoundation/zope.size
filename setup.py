ecccf5ae7cbc6d7cc1022faf4a5036567d204cf6
Tres Seaver <tseaver@palladion.com>
2010-05-05 19:29:05 +0000

    Conform to repository policy.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 ##############################################################################
 #
-# Copyright (c) 2006 Zope Corporation and Contributors.
+# Copyright (c) 2006 Zope Foundation and Contributors.
 # All Rights Reserved.
 #
 # This software is subject to the provisions of the Zope Public License,
@@ -30,7 +30,7 @@ setup(name='zope.size',
       url='http://pypi.python.org/pypi/zope.size',
       license='ZPL 2.1',
       description='Interfaces and simple adapter that give the size of an object',
-      author='Zope Corporation and Contributors',
+      author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       long_description=\
           open('README.txt').read() + \
