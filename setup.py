b4e38f3c5c98e3d2e09a06793feda83164a82f18
Albertas Agejevas <alga@pov.lt>
2013-02-13 17:25:30 +0000

    Preparing release 4.0.0

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@
 from setuptools import setup, find_packages
 
 setup(name='zope.size',
-      version='4.0.0dev',
+      version='4.0.0',
       url='http://pypi.python.org/pypi/zope.size',
       license='ZPL 2.1',
       description=\
